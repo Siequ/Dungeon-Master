2168051ced3a342f94e996723610422acd034872
Siequ <echowojciech@gmail.com>
2024-01-17 20:25:34 +0100

    Making first welcoming messages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Welcome to the game!");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("\nUse up and down arrow keys to choose your class");
+Console.ResetColor();
+while (true)
+{
+Console.WriteLine("Rogue");
+Console.WriteLine("Barbarian");
+Console.WriteLine("Mage");
+
+var userKey = Console.ReadKey(true);
+}
