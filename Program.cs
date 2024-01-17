d0479ba9ceaef9c5f5b9d70ead0723dddc975310
Siequ <echowojciech@gmail.com>
2024-01-17 20:48:56 +0100

    Basic switch to make player choose class

--- Program.cs
+++ Program.cs
@@ -3,11 +3,37 @@ Console.WriteLine("Welcome to the game!");
 Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine("\nUse up and down arrow keys to choose your class");
 Console.ResetColor();
-while (true)
-{
-Console.WriteLine("Rogue");
-Console.WriteLine("Barbarian");
-Console.WriteLine("Mage");
 
-var userKey = Console.ReadKey(true);
+ConsoleKeyInfo userKey;
+int choosedClassOption = 1;
+bool isSelected = false;
+
+while (!isSelected)
+{
+    Console.WriteLine("Rogue");
+    Console.WriteLine("Barbarian");
+    Console.WriteLine("Mage");
+
+    userKey = Console.ReadKey(true);
+
+    switch (userKey.Key)
+    {
+        case ConsoleKey.DownArrow:
+        choosedClassOption++;
+        break;
+
+        case ConsoleKey.UpArrow:
+            choosedClassOption--;
+        break;
+
+        case ConsoleKey.Enter:
+        isSelected = true;
+        break;
+
+
+    }
+    
 }
+
+Console.WriteLine($"You selected {choosedClassOption} ");
+Console.ReadLine();
